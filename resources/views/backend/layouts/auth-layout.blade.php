419e2221c4bb0ffa120bad8b0d1ce39734e7f4fa
ShouoDirector <ivanmilesmvista@gmail,com>
2024-05-01 10:46:49 +0800

    V1.6 - Admin Tabler Integration

---
+++ resources/views/backend/layouts/auth-layout.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>@yield('pageTitle')</title>
+    <!-- CSS files -->
+    <base href="/">
+    <link href="./backend/dist/css/tabler.min.css?1684106062" rel="stylesheet" />
+    <link href="./backend/dist/css/tabler-flags.min.css?1684106062" rel="stylesheet" />
+    <link href="./backend/dist/css/tabler-payments.min.css?1684106062" rel="stylesheet" />
+    <link href="./backend/dist/css/tabler-vendors.min.css?1684106062" rel="stylesheet" />
+    @stack('stylesheets')
+    <link href="./backend/dist/css/demo.min.css?1684106062" rel="stylesheet" />
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+
+    </style>
+</head>
+
+<body class=" d-flex flex-column">
+    <script src="./backend/dist/js/demo-theme.min.js?1684106062"></script>
+    @yield('content')
+    <!-- Libs JS -->
+    <!-- Tabler Core -->
+    <script src="./backend/dist/js/tabler.min.js?1684106062" defer></script>
+    @stack('scripts')
+    <script src="./backend/dist/js/demo.min.js?1684106062" defer></script>
+</body>
+
+</html>
